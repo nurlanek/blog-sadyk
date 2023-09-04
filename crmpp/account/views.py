55d49e9ec4493c814963db281df77f51784cab5f
Nurlanbek <nurlanbek1@gmail.com>
2023-09-04 14:18:49 +0600

    Commit test

--- crmpp/account/views.py
+++ crmpp/account/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            return redirect("home")
+        else:
+            return render(request, "account/login.html", {
+                "error": "username or password is yanlis"
+            })
+
     return render(request, "account/login.html")
 
 
